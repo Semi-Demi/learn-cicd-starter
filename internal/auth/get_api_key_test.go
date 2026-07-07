86a3e73115d24b47cb755de5f8771a1e29575879
Semi-Demi <pvourantonis@hotmail.com>
2026-07-06 21:24:00 -0400

    Adding test with broken code

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,32 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+	"errors"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		headerKey string
+		headerValue string
+		expected string
+		expectedError error
+	}{
+		{"", "", "", ErrNoAuthHeaderIncluded},
+		{"Authorization","", "", ErrNoAuthHeaderIncluded},
+		{"Authorization","token key", "", errors.New("malformed authorization header")},
+		{"Authorization","ApiKey token", "token", nil},
+	}
+
+	for _, test := range tests {
+		header := http.Header{}
+		header.Add(test.headerKey, test.headerValue)
+		value, err :=GetAPIKey(header)
+
+		if value != test.expected || (err != nil && err.Error() != test.expectedError.Error()) || (err == nil && test.expectedError != nil) {
+			t.Errorf("GetAPIKey(%v) = %v, %v; want %v, %v", header, value, err, test.expected, test.expectedError)
+		}
+	}
+
+}
